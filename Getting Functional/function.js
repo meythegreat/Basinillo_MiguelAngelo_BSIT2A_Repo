05f03bb0b4dcaf1ba7ee673424b98571d58a4ae9
meythegreat <miguel102104@gmail.com>
2025-03-20 17:11:59 +0800

    battleship V2 & Functions

---
+++ Getting Functional/function.js
@@ -0,0 +1,45 @@
+// var dogName = "awaw";
+// var dogWeight = 19;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says, WOOF WOOF")
+// } else {
+//     console.log(dogName + " says, woof woof")
+// }
+
+// var dogName = "eloy";
+// var dogWeight = 23;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says, WOOF WOOF")
+// } else {
+//     console.log(dogName + " says, woof woof")
+// }
+
+// var dogName = "khumi";
+// var dogWeight = 12;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says, WOOF WOOF")
+// } else {
+//     console.log(dogName + " says, woof woof")
+// }
+
+// var dogName = "cory";
+// var dogWeight = 35;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says, WOOF WOOF")
+// } else {
+//     console.log(dogName + " says, woof woof")
+// }
+
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF!")
+    } else {
+        console.log(name + " says woof woof!")
+    }
+}
+
+bark("cory", 35);
+bark("awaw", 19);
+bark("khumi", 40);
+bark("eloy", 26);
+bark("elay", 45);
