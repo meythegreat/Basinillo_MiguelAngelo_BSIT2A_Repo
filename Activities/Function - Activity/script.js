97dc321018704465ca7d0aac5b7af41c7aaf88a3
meythegreat <miguel102104@gmail.com>
2025-03-27 17:00:37 +0800

    New Activity!

--- Activities/Function - Activity/script.js
+++ Activities/Function - Activity/script.js
@@ -1,13 +1,36 @@
-function whatShallIWear(temp){
-    if (temp < 60) {
-        console.log("Wear a jacket");
-    } else if (temp < 70) {
-        console.log("Wear a sweater");
+// function whatShallIWear(temp){
+//     if (temp < 60) {
+//         console.log("Wear a jacket");
+//     } else if (temp < 70) {
+//         console.log("Wear a sweater");
+//     } else {
+//         console.log("Wear a t-shirt");
+//     }
+// }
+
+// whatShallIWear(50);
+// whatShallIWear(80);
+// whatShallIWear(60);
+
+function calculateArea(r) {
+    var area;
+    if (r <= 0) {
+        return 0;
     } else {
-        console.log("Wear a t-shirt");
+        area = Math.PI * r * r;
+        return area;
     }
 }
 
-whatShallIWear(50);
-whatShallIWear(80);
-whatShallIWear(60);
+var radius = 10;
+
+var theArea = calculateArea(radius);
+
+console.log("The area is: " + theArea)
+
+function printName(firstName, lastName) {
+    var fullNamed = firstName + ' ' + lastName;
+    return fullNamed
+}
+
+console.log('My full name is ' + printName('Miguel Angelo', 'Basinillo'))
