ecffeb189ea85b60b0fdcdf948f9ef977ee981b2
meythegreat <miguel102104@gmail.com>
2025-05-08 16:39:40 +0800

    Added DOM Activity

---
+++ DOM Activity/script.js
@@ -0,0 +1,9 @@
+document.title = "My Website"
+document.body.style.backgroundColor = "#FFD700"
+
+const username = "";
+const welcomeMsg = document.getElementById("welcome-msg"); 
+
+welcomeMsg.textContent += username === "" ? "Guest!" : username + "!";
+
+console.dir(document)
