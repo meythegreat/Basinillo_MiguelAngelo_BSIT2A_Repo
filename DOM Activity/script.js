dbd691598e5d1f59397d4723b7e1bb639f91ae36
meythegreat <miguel102104@gmail.com>
2025-05-08 16:46:41 +0800

    Additional Edits

--- DOM Activity/script.js
+++ DOM Activity/script.js
@@ -1,7 +1,7 @@
 document.title = "My Website"
 document.body.style.backgroundColor = "#FFD700"
 
-const username = "";
+const username = "Mey the Great";
 const welcomeMsg = document.getElementById("welcome-msg"); 
 
 welcomeMsg.textContent += username === "" ? "Guest!" : username + "!";
